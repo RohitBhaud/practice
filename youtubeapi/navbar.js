14664c1309cbe9192e9d7f1de594e7bbfd98f76b
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-01-15 19:17:03 +0530

    added youtube api app

---
+++ youtubeapi/navbar.js
@@ -0,0 +1,14 @@
+function navbar (){
+    return `    <div id ="navbar">
+    <h3>home</h3>
+
+    <div id="option">
+       <h3><a href="/electronics.html">Electronics</a></h3>
+       <h3><a href="/jewellary.html">Jewellary</a></h3>
+       <h3><a href="">Sign in</a></h3>
+       <h3><a href="">Sign up</a></h3>
+    </div>
+</div>`
+}
+
+export default navbar
